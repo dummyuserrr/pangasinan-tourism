cb491c92154d6bb09c2c836c6d2335be7a2eb793
Deither <dthrcrpz@gmail.com>
2018-02-07 16:08:54 +0800

    started the it

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -29,19 +29,16 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ml-auto">
                         <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+                            <a class="nav-link" href="/the-province">The Province</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                            <a class="nav-link" href="/tourism">Tourism</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                            <a class="nav-link" href="/find-us">Find Us</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#team">Team</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                            <a class="nav-link js-scroll-trigger" href="#contact">Contact Us</a>
                         </li>
                     </ul>
                 </div>
