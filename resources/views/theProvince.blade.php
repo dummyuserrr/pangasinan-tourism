11e79e207812eee0784e3ca1359c87870dfcca61
Deither <dthrcrpz@gmail.com>
2018-02-09 18:33:22 +0800

    done with modal with multiple images

--- resources/views/theProvince.blade.php
+++ resources/views/theProvince.blade.php
@@ -211,7 +211,7 @@
 		</div>
 		<div class="row">
 			<div class="col-md-4 col-sm-6 portfolio-item">
-				<a class="portfolio-link" data-toggle="modal" href="#imageModal">
+				<a class="portfolio-link" data-toggle="modal" href="#galleryModal">
 					<div class="portfolio-hover">
 						<div class="portfolio-hover-content">
 							<i class="fa fa-eye fa-3x"></i>
@@ -225,7 +225,7 @@
 				</div>
 			</div>
 			<div class="col-md-4 col-sm-6 portfolio-item">
-				<a class="portfolio-link" data-toggle="modal" href="#imageModal">
+				<a class="portfolio-link" data-toggle="modal" href="#galleryModal">
 					<div class="portfolio-hover">
 						<div class="portfolio-hover-content">
 							<i class="fa fa-eye fa-3x"></i>
@@ -239,7 +239,7 @@
 				</div>
 			</div>
 			<div class="col-md-4 col-sm-6 portfolio-item">
-				<a class="portfolio-link" data-toggle="modal" href="#imageModal">
+				<a class="portfolio-link" data-toggle="modal" href="#galleryModal">
 					<div class="portfolio-hover">
 						<div class="portfolio-hover-content">
 							<i class="fa fa-eye fa-3x"></i>
@@ -253,7 +253,7 @@
 				</div>
 			</div>
 			<div class="col-md-4 col-sm-6 portfolio-item">
-				<a class="portfolio-link" data-toggle="modal" href="#imageModal">
+				<a class="portfolio-link" data-toggle="modal" href="#galleryModal">
 					<div class="portfolio-hover">
 						<div class="portfolio-hover-content">
 							<i class="fa fa-eye fa-3x"></i>
@@ -267,7 +267,7 @@
 				</div>
 			</div>
 			<div class="col-md-4 col-sm-6 portfolio-item">
-				<a class="portfolio-link" data-toggle="modal" href="#imageModal">
+				<a class="portfolio-link" data-toggle="modal" href="#galleryModal">
 					<div class="portfolio-hover">
 						<div class="portfolio-hover-content">
 							<i class="fa fa-eye fa-3x"></i>
@@ -281,7 +281,7 @@
 				</div>
 			</div>
 			<div class="col-md-4 col-sm-6 portfolio-item">
-				<a class="portfolio-link" data-toggle="modal" href="#imageModal">
+				<a class="portfolio-link" data-toggle="modal" href="#galleryModal">
 					<div class="portfolio-hover">
 						<div class="portfolio-hover-content">
 							<i class="fa fa-eye fa-3x"></i>
@@ -308,20 +308,39 @@
 			</div>
 			<div class="container">
 				<div class="row">
-					<div class="col-lg-8 mx-auto">
+					<div class="col-lg-12 mx-auto">
 						<div class="modal-body">
 							<h2 class="text-uppercase">Project Name</h2>
 							<p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-							<img class="img-fluid d-block mx-auto" src="img/portfolio/01-full.jpg" alt="">
+							<div class="multiple_images">
+								<div class="box"><img src="/img/portfolio/01-full.jpg"></div>
+								<div class="box"><img src="/img/portfolio/01-full.jpg"></div>
+								<div class="box"><img src="/img/portfolio/01-full.jpg"></div>
+							</div>
 							<p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-							<ul class="list-inline">
-								<li>Date: January 2017</li>
-								<li>Client: Threads</li>
-								<li>Category: Illustration</li>
-							</ul>
-							<button class="btn btn-primary" data-dismiss="modal" type="button">
-							<i class="fa fa-times"></i>
-							Close Project</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="portfolio-modal modal fade" id="galleryModal" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="close-modal" data-dismiss="modal">
+				<div class="lr">
+					<div class="rl"></div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 mx-auto">
+						<div class="modal-body">
+							<h2 class="text-uppercase">Project Name</h2>
+							<p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+							<img class="img-fluid d-block mx-auto" src="img/portfolio/06-full.jpg" alt="">
+							<p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
 						</div>
 					</div>
 				</div>
