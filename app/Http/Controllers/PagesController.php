9922a302b80c64bc97d48cded3d228eeb32161ee
Deither Corpuz <dthrcrpz@gmail.com>
2018-02-10 14:34:22 +0800

    added title

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,18 +7,22 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-    	return view('index');
+        $title = 'Pangasinan Tourism';
+    	return view('index', compact('title'));
     }
 
     public function theProvince(){
-    	return view('theProvince');
+        $title = 'The Province - Pangasinan Tourism';
+    	return view('theProvince', compact('title'));
     }
 
     public function tourism(){
-    	return view('tourism');
+        $title = 'Tourism - Pangasinan Tourism';
+    	return view('tourism', compact('title'));
     }
 
     public function findUs(){
-    	return view('findUs');
+        $title = 'Find Us - Pangasinan Tourism';
+    	return view('findUs', compact('title'));
     }
 }
