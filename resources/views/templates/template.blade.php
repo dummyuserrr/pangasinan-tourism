90374ef0303351d8e94388c6761c8f1273ebf7e9
Deither <dthrcrpz@gmail.com>
2018-02-19 19:09:52 +0800

    uwian na

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -149,6 +149,19 @@
         <script src="/js/contact_me.js"></script>
         <!-- Custom scripts for this template -->
         <script src="/js/agency.min.js"></script>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/5a8aafe9d7591465c707cab2/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+        </script>
+        <!--End of Tawk.to Script-->
         <script src="/js/actions.js"></script>
     </body>
 </html>
