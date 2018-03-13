9e59cefc1351d7b23e536702a6971d7cefecba37
Wala <dummyemail@email.cc>
2018-03-13 11:47:27 +0800

    fix on client

---
+++ storage/framework/views/0ef83451d5774d343c9dabf449dcacefdac0318b.php
@@ -0,0 +1,62 @@
+<?php $__env->startSection('content'); ?>
+<button type="button" class="btn btn-warning" onclick="redirect('/admin-panel/the-province/cities-and-municipalities')"><i class="fa fa-list" aria-hidden="true"></i> Return to list</button>
+<div class="white_container">
+	<form method="post" action="/admin-panel/the-province/cities-and-municipalities/<?php echo e($cm->id); ?>/update" enctype="multipart/form-data">
+		<?php echo e(csrf_field()); ?>
+
+		<?php echo e(method_field('patch')); ?>
+
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="form-group">
+					<label for="name">Name:</label>
+					<input type="text" name="name" class="form-control" id="name" required value="<?php echo e($cm->name); ?>">
+				</div>
+				<div class="form-group">
+					<label for="description">Description:</label>
+					<textarea type="text" class="form-control" name="description" rows="7" required><?php echo e($cm->description); ?></textarea>
+				</div>
+				<button type="submit" class="btn btn-primary">Save</button>
+			</div>
+			<div class="col-lg-4">
+				<div class="form-group">
+					<label for="otherDetails">Images:</label>
+					<div class="thumbnails">
+						<?php $__currentLoopData = $cm->images; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $image): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+						<div class="box cmsImageBox citymunImage" title="Click to view or delete image" style="background-image: url('/<?php echo e($image->path); ?>');" data-toggle="modal" data-target="#imageModal" data-url="/admin-panel/the-province/cities-and-municipalities/image/<?php echo e($image->id); ?>/delete">
+							<img src="/<?php echo e($image->path); ?>">
+						</div>
+						<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="images">Images: (Optional | You can upload multiple)</label>
+					<input type="file" class="form-control" name="images[]" multiple>
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
+<div id="imageModal" class="modal fade" role="dialog">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-body text-center">
+				<img src="/">
+			</div>
+			<div class="modal-footer">
+				<?php if($cm->images->count() > 1): ?>
+				<button type="button" class="btn btn-danger initiateDelete"><i class="fa fa-trash"></i> Delete this Image</button>
+				<?php endif; ?>
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+<form id="deleteForm" method="post" action="">
+	<?php echo e(csrf_field()); ?>
+
+	<?php echo e(method_field('delete')); ?>
+
+</form>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('adminpanel.template', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/1f303903ca40ba00e1b1a6c98e0a47de324b8ccc.php
@@ -0,0 +1,13 @@
+<?php $__env->startSection('content'); ?>
+<!-- Header -->
+<header class="masthead">
+	<div class="container">
+		<div class="intro-text">
+			<div class="intro-lead-in">Welcome To Pangasinan!</div>
+			<div class="intro-heading text-uppercase">Welcome Message Here</div>
+			<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="/the-province">Tell Me More</a>
+		</div>
+	</div>
+</header>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('templates.template', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
