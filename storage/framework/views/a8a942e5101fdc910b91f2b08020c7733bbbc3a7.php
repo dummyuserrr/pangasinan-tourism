5ba2fc1a6d996a7791508e4056a5a7053f6e0cf8
wala <waala@wa.cwala>
2018-03-15 14:25:22 +0800

    for merge

---
+++ storage/framework/views/a8a942e5101fdc910b91f2b08020c7733bbbc3a7.php
@@ -0,0 +1,62 @@
+<?php $__env->startSection('content'); ?>
+<button type="button" class="btn btn-warning" onclick="redirect('/admin-panel/tourism/tourist-attractions')"><i class="fa fa-list" aria-hidden="true"></i> Return to list</button>
+<div class="white_container">
+	<form method="post" action="/admin-panel/tourism/tourist-attractions/<?php echo e($ta->id); ?>/update" enctype="multipart/form-data">
+		<?php echo e(csrf_field()); ?>
+
+		<?php echo e(method_field('patch')); ?>
+
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="form-group">
+					<label for="name">Name: <span class="req">*</span></label>
+					<input type="text" class="form-control" id="name" name="name" value="<?php echo e($ta->name); ?>">
+				</div>
+				<div class="form-group">
+					<label for="content">Description:</label>
+					<textarea type="text" class="form-control" rows="5" name="description"><?php echo e($ta->description); ?></textarea>
+				</div>
+				<button type="submit" class="btn btn-primary">Save</button>
+			</div>
+			<div class="col-lg-4">
+				<div class="form-group">
+					<label for="otherDetails">Images:</label>
+					<div class="thumbnails">
+						<?php $__currentLoopData = $ta->images; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $image): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+						<div class="box cmsImageBox" title="Click to view or delete image" style="background-image: url('/<?php echo e($image->path); ?>');" data-toggle="modal" data-target="#imageModal" data-url="/admin-panel/tourism/tourist-attractions/<?php echo e($image->id); ?>/delete-image">
+							<img src="/<?php echo e($image->path); ?>">
+						</div>
+						<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="title">Image:</label>
+					<input type="file" name="image[]" multiple class="form-control postImage">
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
+<div id="imageModal" class="modal fade" role="dialog">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-body text-center">
+				<img src="/">
+			</div>
+			<div class="modal-footer">
+				<?php if($ta->images->count() > 1): ?>
+				<button type="button" class="btn btn-danger initiateDelete"><i class="fa fa-trash"></i> Delete this Image</button>
+				<?php endif; ?>
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+<form id="deleteForm" method="post" action="">
+	<?php echo e(csrf_field()); ?>
+
+	<?php echo e(method_field('delete')); ?>
+
+</form>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('adminpanel.template', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/bc6712893a9f7818d90411ca30d01ae3c3b80952.php
@@ -0,0 +1,29 @@
+<?php $__env->startSection('content'); ?>
+<button type="button" class="btn btn-warning" onclick="redirect('/admin-panel/tourism/festivals')"><i class="fa fa-list" aria-hidden="true"></i> Return to list</button>
+<div class="white_container">
+	<form method="post" action="/admin-panel/tourism/festivals/new" enctype="multipart/form-data">
+		<?php echo e(csrf_field()); ?>
+
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="form-group">
+					<label for="name">Name: <span class="req">*</span></label>
+					<input type="text" class="form-control" id="name" name="name">
+				</div>
+				<div class="form-group">
+					<label for="content">Description:</label>
+					<textarea type="text" class="form-control" rows="5" name="description"></textarea>
+				</div>
+				<button type="submit" class="btn btn-primary">Save</button>
+			</div>
+			<div class="col-lg-4">
+				<div class="form-group">
+					<label for="title">Images:</label>
+					<input type="file" name="image[]" multiple required class="form-control postImage">
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('adminpanel.template', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
