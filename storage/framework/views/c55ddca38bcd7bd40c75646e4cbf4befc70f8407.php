0a6b6c2939329b7ec77d98a708b829d3ce83c7d6
Deither Corpuz <dthrcrpz@gmail.com>
2018-03-24 13:14:41 +0800

    cleared views

--- storage/framework/views/c55ddca38bcd7bd40c75646e4cbf4befc70f8407.php
+++
@@ -1,7 +0,0 @@
-<h3 class="text-uppercase"><?php echo e($delicacy->name); ?></h3>
-<div class="multiple_images">
-	<?php $__currentLoopData = $delicacy->images; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $image): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-	<div class="box"><img src="/<?php echo e($image->path); ?>"></div>
-	<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-</div>
-<p><?php echo e($delicacy->description); ?></p>
