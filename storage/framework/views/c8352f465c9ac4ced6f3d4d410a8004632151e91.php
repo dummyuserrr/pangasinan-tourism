67c072ffe075bfed9bc8339eb125bd22617f05d9
wala <waala@wa.cwala>
2018-03-26 11:01:29 +0800

    latest from c

---
+++ storage/framework/views/c8352f465c9ac4ced6f3d4d410a8004632151e91.php
@@ -0,0 +1,28 @@
+<div class="row">
+	<div class="col-lg-5"><img src="/<?php echo e($mv->images()->orderBy('created_at', 'desc')->first()->path); ?>" class="mw100"></div>
+	<div class="col-lg-7">
+		<h3 class="text-uppercase"><?php echo e($mv->name); ?></h3>
+		<p><?php echo e($mv->description); ?></p>
+	</div>
+</div>
+<?php if($mv->category != 'delicacies'): ?>
+<div class="row">
+	<div class="col-lg-12">
+		<div id="map" class="cmMap"></div>
+	</div>
+</div>
+<?php endif; ?>
+<div class="row">
+	<div class="col-lg-12">
+		<h3 class="text-uppercase mt">Photo Gallery</h3>
+		<div class="row">
+			<?php $__currentLoopData = $mv->images()->orderBy('created_at', 'desc')->get(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $image): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+				<?php if($loop->first): ?>
+				
+				<?php else: ?>
+					<div class="col-lg-4"><img src="/<?php echo e($image->path); ?>" class="mw100"></div>
+				<?php endif; ?>
+			<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+		</div>
+	</div>
+</div>
